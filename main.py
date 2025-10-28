5ee98884badfcce125ddfbbdb60c385543c82d03
unknown <matheus.moreira.marcelino.dev@gmail.com>
2025-10-28 12:10:32 -0300

    criando a coluna de senha na tabela biblioteca e iniciando o processo de criptografia

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 from fastapi import FastAPI
+from passlib.context import CryptContext
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 app = FastAPI()
 
+bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
 from routers.livro_routers import livro_router
+from routers.biblioteca_routers import biblioteca_router
+
 app.include_router(livro_router)
+app.include_router(biblioteca_router)
 
 @app.get("/")
 async def home():
--- routers/biblioteca_routers.py
+++ routers/biblioteca_routers.py
@@ -1,6 +1,7 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from models import Biblioteca
 from dependencies import get_session
+from main import bcrypt_context
 
 biblioteca_router = APIRouter(prefix="/bibliotecas", tags=["bibliotecas"])
 
@@ -10,4 +11,9 @@ async def home():
 
 @biblioteca_router.post("/cadastrar")
 async def register(nome: str, email: str, senha:str, session  = Depends(get_session)):
-    return
+    senha_criptografada = bcrypt_context.hash(senha)
+    
+    nova_biblioteca = Biblioteca(nome, senha_criptografada, email)
+    session.add(nova_biblioteca)
+    session.commit()
+    return {"mensagem":"biblioteca cadastrada!"}
